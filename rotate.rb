8aa57f28c862e8430713b336e4e81acb4567098f
ayush-amura <ayush@amuratech.com>
2017-08-11 12:59:12 +0530

    rotate.rb

---
+++ rotate.rb
@@ -0,0 +1,10 @@
+# Write a method that rotates a list by 'k' elements. 
+# For example [1,2,3,4,5,6] rotated by 'two' becomes [3,4,5,6,1,2]. Try solving this without creating a copy of the list.
+
+
+def rotate (arr,k)
+
+ rotated=arr.rotate(k)
+p rotated
+end
+rotate([1,2,3,4,5,6],2)
