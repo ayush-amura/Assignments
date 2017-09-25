6daed8efef0b81100202647270d67efc9cc9f519
AYUSH <ayush@amuratech.com>
2017-09-25 13:03:00 +0530

    tree & tags

--- html_parser/parser.rb
+++ html_parser/parser.rb
@@ -1,19 +1,69 @@
-file = File.open("input.html","r")
-content=[]
-a=[]
-file.each_line do |line|
-  a << line.chomp
-  content=a.to_s
+$prompt = '> '
+class Parser
+def introduction
+	puts"====================================="
+	puts "-----------HTML - PARSER------------"
+	puts"====================================="
+	puts " "
+	menu
 end
+
+def menu
+file_read 
+	puts "1.HTML Tree"
+	puts "2.HTML Tags"
+	puts "3.HTML Source file"
+	print "Enter Your choice #{$prompt}"
+	choice=gets.chomp
+	case choice
+		when "1"
+  		tree_html
+  	when "2"
+  		tag_html
+  	when "3"
+  		file_html
+  	else
+    	puts "Choose appropriate"
+    end
 	
-print "Html File ::\n"
-puts a 
-puts "" 
+end
+		
+def file_read
+	file = File.open("input.html","r")
+	@content=[]
+	@html_file=[]
+	file.each_line do |line|
+	  @html_file << line.chomp
+	  @content=@html_file.to_s
+	end
+	
+end
+
+def file_html
+	print "Html File ::\n"
+	puts @html_file 
+	puts "" 
+end
+
+
+def tree_html
 	key = /(<(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>)/
-	
-	m = content.scan(key) 
-	puts "All tags in Html File are ::"
-	puts m
+	tree = @content.scan(key) 
+	puts "Html Tree ::"
+	puts tree
+	puts "----------------------------------------------"
+	menu
+end
 
-puts if(m[0]==m.last)
+def tag_html
+	key_tag= /(<[a-z]+\s{0,1}>)/
+	tags = @content.scan(key_tag) 
+	puts "Tags in HTML file are ::"
+	puts tags
+	puts "----------------------------------------------"
+	menu
+end
+end
 
+p = Parser.new
+p.introduction
